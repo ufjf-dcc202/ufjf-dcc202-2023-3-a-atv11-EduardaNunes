2ff15664c61ba8cb7c6676ff875f066a65de1898
Duda Nunes <eduardapmn@gmail.com>
2023-12-05 00:43:05 -0300

    funçao removeDaLista adicionada

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getLista, limpaLista, adicionaNaLista} from "./lista.js"
+import { getLista, limpaLista, adicionaNaLista, removeDaLista} from "./lista.js"
 
 const olItens = document.querySelector('#itens');
 const pEntrada = document.querySelector('#entrada');
